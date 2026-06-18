f8b8fb0e573e3c8e0865914f215116f49911cc86
Farjana Jahan Anika <anikafarjanajahan@gmail.com>
2026-06-18 16:50:17 +0600

    Update Main7.java

--- Task 2/Main7.java
+++ Task 2/Main7.java
@@ -1,1 +1,19 @@
+package Labtasks;
+
+interface clickable{
+   void onClick();
+}
+class Button implements clickable{
+    public void onClick(){
+        System.out.println("Button was clicked!");
+    }
+
+}
+
+public class Main7 {
+    public static void main(String[]args){
+        Button btn=new Button();
+        btn.onClick();
+    }
+}
 
