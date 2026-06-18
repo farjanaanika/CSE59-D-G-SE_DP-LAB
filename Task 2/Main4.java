ca348bc35a8b52a8111866e3ecaa06b58d230a5f
Farjana Jahan Anika <anikafarjanajahan@gmail.com>
2026-06-18 16:48:32 +0600

    Update Main4.java

--- Task 2/Main4.java
+++ Task 2/Main4.java
@@ -1,1 +1,22 @@
-
+package Labtasks;
+ class Sensor{
+     String name;
+     int batteryLevel;
+     
+     Sensor(String name){
+         this.name=name;
+         this.batteryLevel=0;
+     }
+     void charge(){
+         batteryLevel=100;
+         System.out.println("Sensor fully charged");
+     }
+ }
+public class Main4 {
+    public static void main(String[]args){
+        Sensor sensor =new Sensor("Temp Sensor");
+        sensor.charge();
+        System.out.println("Battery level: "+sensor.batteryLevel);
+    }
+    
+}
