3c3bb653f03d4f7539d90cdd64078beb7ecf5841
Farjana Jahan Anika <anikafarjanajahan@gmail.com>
2026-07-02 16:32:30 +0600

    Update Main1.java

--- Task 4/Main1.java
+++ Task 4/Main1.java
@@ -1,1 +1,43 @@
+package lab04;
 
+
+public class Main1 {
+    static class TeamMember{
+        protected String name;
+        public TeamMember(String name){
+          this.name=name;
+        }
+        public void work(){
+            System.out.println(name + "is working.");
+        }
+    }
+    
+    static class Employee extends TeamMember{
+       public Employee(String name){
+           super(name);
+       } 
+       public void calculateBonus(){
+           System.out.println( "calculating standard employee bonus...");
+       }
+    }
+    
+    static class Contractor extends TeamMember{
+        public Contractor(String name){
+        super(name);
+        }
+        public void submitInvoice(){
+        System.out.println( name+ " submitted an invoice for payment.");
+        }
+    }
+    
+    public static void main (String[]args){
+    Employee emp=new Employee("Alice");
+    Contractor cont=new Contractor("Bob");
+    
+    emp.work();
+    emp.calculateBonus();
+    cont.work();
+    cont.submitInvoice();
+    
+    }
+}
